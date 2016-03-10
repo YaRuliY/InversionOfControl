ca78d5b12149f5f12bfd75450cdac2471948c55a
Yaroslav Lukyanenko <l.yaruk1993@gmail.com>
2016-03-10 17:53:26 +0200

    lab1, task1-2

--- sandboxedModule/ru/application.js
+++ sandboxedModule/ru/application.js
@@ -1,17 +1,15 @@
-// Файл содержит маленький кусочек основного модуля демонстрационного
-// прикладного приложения, загружаемого в песочницу демонстрационным
-// кусочком фреймворка. Читайте README.md в нем задания.
-
 console.log('From application global context');
 
-module.exports = function() {
+module.exports = {};
+module.exports.action = function() {
     console.log('From application exported function');
 
-    var time = function(time){
-        setTimeout(function(){
-            console.log('in interval');
-        },time*1000);
-    };
-
-    time(1);
+    console.log('isArray([1,2,4]) -> ' + util.isArray([1,2,4]));
+    var interval = setInterval(function(){
+        console.log("in interval");
+    }, 1000);
+    setTimeout(function(){
+        clearInterval(interval);
+        console.log("the end");
+    }, 3000);
 };
--- sandboxedModule/ru/framework.js
+++ sandboxedModule/ru/framework.js
@@ -1,34 +1,22 @@
-// Файл, демонстрирующий то, как фреймворк создает среду (песочницу) для
-// исполнения приложения, загружает приложение, передает ему песочницу в
-// качестве глобального контекста и получает ссылу на экспортируемый
-// приложением интерфейс. Читайте README.md в нем задания.
-
-// Фреймворк может явно зависеть от библиотек через dependency lookup
 var fs = require('fs'),
     vm = require('vm'),
     util = require('util');
 
-// Создаем контекст-песочницу, которая станет глобальным контекстом приложения
 var context = {
     module: {},
     console: console,
     setInterval: setInterval,
     setTimeout: setTimeout,
+    clearInterval: clearInterval,
     util: util
 };
 context.global = context;
 var sandbox = vm.createContext(context);
 
-// Читаем исходный код приложения из файла
 var fileName = './application.js';
 fs.readFile(fileName, function(err, src) {
-  // Тут нужно обработать ошибки
-  
-  // Запускаем код приложения в песочнице
-  var script = vm.createScript(src, fileName);
-
-  script.runInNewContext(sandbox);
-  
-  // Забираем ссылку из sandbox.module.exports, можем ее исполнить,
-  // сохранить в кеш, вывести на экран исходный код приложения и т.д.
+    var script = vm.createScript(src, fileName);
+    script.runInNewContext(sandbox);
+    var modula = sandbox.module.exports;
+    modula.action();
 });

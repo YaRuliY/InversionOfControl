ef39957bc0944849d0e08733dcd91870955c5952
Lukyanenko Yaroslav <l.yaruk1993@gmail.com>
2016-03-26 17:00:07 +0200

    task 1-6

--- interfaceWrapper/ru/application.js
+++ interfaceWrapper/ru/application.js
@@ -7,3 +7,20 @@ console.log('Application going to read ' + fileName);
 fs.readFile(fileName, function(err, src) {
   console.log('File ' + fileName + ' size ' + src.length);
 });
+
+var logger = fs.createWriteStream('./file.log', {flags: 'r+'});
+logger.write('text');
+
+/*var reader = fs.createReadStream('./file.log');*/
+
+setTimeout(function(){
+  console.log('out in app');
+  var reader = fs.createReadStream('./file.log');
+  var data = '';
+  reader.on('data', function(param) {
+    data+=param;
+  });
+  reader.on('end', function() {
+    console.log('logfile: ' + data);
+  });
+}, 1000);
--- interfaceWrapper/ru/framework.js
+++ interfaceWrapper/ru/framework.js
@@ -7,34 +7,30 @@ var fs = require('fs'),
 var context = {
   module: {},
   console: console,
-  // Помещаем ссылку на fs API в песочницу
   fs: cloneInterface(fs),
-  // Оборачиваем функцию setTimeout в песочнице
   setTimeout: function(callback, timeout) {
-    // Добавляем поведение при вызове setTimeout
     console.log(
       'Call: setTimeout, ' +
       'callback function: ' + callback.name + ', ' +
       'timeout: ' + timeout
     );
     setTimeout(function() {
-      // Добавляем поведение при срабатывании таймера
+      var d1 = Date.now();
       console.log('Event: setTimeout, before callback');
-      // Вызываем функцию пользователя на событии таймера
       callback();
       console.log('Event: setTimeout, after callback');
+      var d2 = Date.now();
+      var d3 = d2-d1;
+      console.log('time of execution: ' + d3);
     }, timeout);
   }
 };
 
-// Преобразовываем хеш в контекст
 context.global = context;
 var sandbox = vm.createContext(context);
 
-// Читаем исходный код приложения из файла
 var fileName = './application.js';
 fs.readFile(fileName, function(err, src) {
-  // Запускаем код приложения в песочнице
   var script = vm.createScript(src, fileName);
   script.runInNewContext(sandbox);
 });
@@ -51,14 +47,13 @@ function wrapFunction(fnName, fn) {
   return function wrapper() {
     var args = [];
     Array.prototype.push.apply(args, arguments);
-    console.log('before if');
     if (typeof(args[args.length - 1]) === 'function'){
       var func = args[args.length - 1];
       args[args.length - 1] = wrapFunction(args[args.length - 1].name, args[args.length - 1]);
       console.log('arguments - >' + args);
     }
     console.log('Call: ' + fnName);
-    console.dir(args);
+    //console.dir(args);
     return fn.apply(undefined, args);
   }
 }
